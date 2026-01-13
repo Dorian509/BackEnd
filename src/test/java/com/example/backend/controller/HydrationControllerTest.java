f1b1f87d37e6dcaec6f59441c454d63cbf76e0bf
Dorian Gutsche <s0596915@htw-berlin.de>
2026-01-13 10:38:08 +0100

    Cleanup

--- src/test/java/com/example/backend/controller/HydrationControllerTest.java
+++ src/test/java/com/example/backend/controller/HydrationControllerTest.java
@@ -14,7 +14,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -30,7 +30,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Testet die wichtigsten REST-Endpoints.
  */
 @WebMvcTest(HydrationController.class)
-class HydrationControllerTest {
+class
+HydrationControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -38,7 +39,7 @@ class HydrationControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private HydrationService hydrationService;
 
     @Test
